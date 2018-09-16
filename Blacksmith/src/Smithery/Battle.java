b7c1e9bdef4f4069e9419ca45b2f3d0e0460ad40
ueberrushung <ericsimonseidler@gmail.com>
2018-09-16 14:03:39 +0200

    init gameloop

--- Blacksmith/src/Smithery/Battle.java
+++ Blacksmith/src/Smithery/Battle.java
@@ -7,7 +7,7 @@ public class Battle {
 	
 	public Monster monster;
 	public Attacker attacker;
-	private static Timer timer;
+	
 	
 	public Battle( Attacker attacker, Monster monster) {
 		this.attacker = attacker;
@@ -19,12 +19,8 @@ public class Battle {
 		this.attacker.weapon.print();
 		System.out.println("");
 		System.out.println("Monster has: "+  this.monster.hp + " HP");
-		while( this.monster.alive() && this.attacker.alive() ) {
-			startTimer("peter");
-			resetSomeTimer();
+		while( this.monster.alive() && this.attacker.alive() ) {		
 			attack();
-			//timer.schedule(new Hit(), (long)(1000 * this.attacker.weapon.attSpeed));
-			timer.cancel();
 		}
 	}
 	
@@ -33,31 +29,4 @@ public class Battle {
 		System.out.println("Bob attacks with: " + this.attacker.weapon.totalDMG + " DMG" );
 		System.out.println(this.monster.name + " HP: " + this.monster.hp);
 	}
-	
-	private void startTimer(String name) {
-        TimerTask timerTask = new TimerTask() {
-
-            @Override
-            public void run() {
-                System.out.println("name: " + name);
-            }
-        };
-        timer = new Timer();
-        timer.schedule(timerTask, 5000);
-
-    }
-
-    public void resetSomeTimer() {
-        TimerTask timerTask = new TimerTask() {
-
-            @Override
-            public void run() {
-                System.out.println("updating timer");
-            }
-        };
-        timer.cancel();
-        timer = new Timer();
-        timer.schedule(timerTask, 1000);
-
-    }
 }
